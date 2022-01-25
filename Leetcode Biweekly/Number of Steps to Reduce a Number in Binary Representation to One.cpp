834d6a5a0528854004dcb537dd1d5db6102ae953
harshulgarg <56495746+harshul21@users.noreply.github.com>
2022-01-25 15:28:56 +0530

    Create Number of Steps to Reduce a Number in Binary Representation to One.cpp

---
+++ Leetcode Biweekly/Number of Steps to Reduce a Number in Binary Representation to One.cpp
@@ -0,0 +1,28 @@
+//Problem Link:-https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+class Solution {
+public:
+    int numSteps(string s) {
+        int step=0;
+        while(s.size()>1){
+            if(s.back()=='0'){
+                s.pop_back();
+                step++;
+                continue;
+            }
+            bool check=false;
+            for(int i=s.size()-1;i>=0;i--){
+                if(s[i]=='0'){
+                    s[i]='1';
+                    check=true;
+                    break;
+                }
+                s[i]='0';
+            }
+            if(!check){
+                s="1"+s;
+            }
+            step++;
+        }
+        return step;
+    }
+};
